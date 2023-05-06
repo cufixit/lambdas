82688c7fae7b5112af1ff25bbff9d0f8e5e29fd7
Ethan Wu <ew2664@columbia.edu>
2023-05-06 11:20:45 -0400

    Add lambda to setup opensearch indexes

---
+++ domain-setup/lambda_function.py
@@ -0,0 +1,58 @@
+import os
+import cfnresponse
+from opensearchpy import OpenSearch, RequestsHttpConnection
+
+DOMAIN_ENDPOINT = os.environ["DOMAIN_ENDPOINT"]
+DOMAIN_PORT = os.environ.get("DOMAIN_PORT", 443)
+MASTER_USERNAME = os.environ["MASTER_USERNAME"]
+MASTER_PASSWORD = os.environ["MASTER_PASSWORD"]
+
+CREATE = "Create"
+UPDATE = "Update"
+response_data = {}
+
+search = OpenSearch(
+    hosts=[{"host": DOMAIN_ENDPOINT, "port": DOMAIN_PORT}],
+    http_auth=(MASTER_USERNAME, MASTER_PASSWORD),
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection,
+)
+
+
+def lambda_handler(event, context):
+    print(f"Received event: {event}")
+
+    try:
+        if event["RequestType"] == CREATE:
+            if not search.indices.exists("reports"):
+                response = search.indices.create(
+                    "reports",
+                    body={
+                        "mappings": {
+                            "properties": {
+                                "reportID": {"type": "keyword"},
+                                "userID": {"type": "keyword"},
+                                "groupID": {"type": "keyword"},
+                            }
+                        }
+                    },
+                )
+                print(f"Successfully created reports index: {response}")
+            if not search.indices.exists("groups"):
+                response = search.indices.create(
+                    "groups",
+                    body={
+                        "mappings": {
+                            "properties": {
+                                "groupID": {"type": "keyword"},
+                            }
+                        }
+                    },
+                )
+                print(f"Successfully created groups index: {response}")
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, response_data)
+
+    except Exception as error:
+        print(f"Error: {error}")
+        cfnresponse.send(event, context, cfnresponse.FAILED, response_data)
