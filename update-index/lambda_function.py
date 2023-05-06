d3dba349b37dc244508d4bfb8f05fc990f3bfa1e
jesse-woo <114460358+jesse-woo@users.noreply.github.com>
2023-05-05 23:54:52 -0400

    vectorize records
    Added vectorization of records based on keywords and location code. Does not index the vector yet.

--- update-index/lambda_function.py
+++ update-index/lambda_function.py
@@ -3,6 +3,7 @@ import json
 import boto3
 from opensearchpy import OpenSearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
+from sklearn.feature_extraction.text import CountVectorizer
 
 AWS_REGION = os.environ["AWS_REGION"]
 REPORTS_DOMAIN_HOST = os.environ["reportsDomainHost"]
@@ -24,6 +25,14 @@ search = OpenSearch(
     connection_class=RequestsHttpConnection,
 )
 
+s3 = boto3.client('s3')
+bucket_name = 'cu-fixit-ml'
+object_key = 'vocabulary.txt'
+
+# Load the vocabulary from the S3 bucket
+response = s3.get_object(Bucket=bucket_name, Key=object_key)
+vocab_file_content = response['Body'].read().decode('utf-8')
+vocabulary = set(vocab_file_content.split('\n'))
 
 def lambda_handler(event, context):
     print(f"Received event: {event}")
@@ -66,19 +75,40 @@ def lambda_handler(event, context):
                         "status": new_image["status"]["S"],
                         "date": new_image["date"]["S"],
                     }
+
+                    for attr in ["keywords", "photoLabels"]:
+                        if attr in new_image:
+                            words = new_image[attr]["SS"]
+                            for word in words:
+                                vocabulary.add(word.lower())
+                    
+                    vectorizer = CountVectorizer(vocabulary=vocabulary)
+                    # Use the existing checks to build the sequence to vectorize
+                    record_text = f"{new_image['title']['S']}, {new_image['location']['S']}"
+
                     if "groupID" in new_image:
                         body["groupID"] = new_image["groupID"]["S"]
                     if "keywords" in new_image:
                         body["keywords"] = " ".join(new_image["keywords"]["SS"])
+                        record_text += f", {', '.join(new_image['keywords']['SS'])}"
                     if "photoLabels" in new_image:
                         body["photoLabels"] = " ".join(new_image["photoLabels"]["SS"])
+                        record_text += f", {', '.join(new_image['photoLabels']['SS'])}"
+                    
+                    X = vectorizer.fit_transform([record_text])
+                    print(f"The vector for report {id} is {X}")
                     print(f"Adding {id} to reports index: {body}")
                     search.index(index="reports", id=id, body=body)
+                    #TODO index the vector
 
             documents_counts += 1
 
         except Exception as error:
             print(f"Error processing record: {error}")
+
+    sorted_vocabulary = sorted(vocabulary)
+    vocab_file_content = '\n'.join(sorted_vocabulary)
+    s3.put_object(Bucket=bucket_name, Key=object_key, Body=vocab_file_content)
 
     return {
         "statusCode": 200,
